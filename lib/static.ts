04312ae881f301bb3ee8eaa94d63e6c392fb86a4
Flysky12138 <x2797299614@gmail.com>
2024-03-21 13:53:02 +0800

    feat: add item

--- lib/static.ts
+++ lib/static.ts
@@ -1,6 +1,7 @@
 export const LIST = [
   { url: 'https://www.amazon.com/gp/new-releases/fashion/679416011', category: 'Pumps' },
-  { url: 'https://www.amazon.com/gp/new-releases/fashion/679425011', category: 'Sandals' },
+  { url: 'https://www.amazon.com/gp/new-releases/fashion/10509650011', category: 'Heeled Sandals' },
   { url: 'https://www.amazon.com/gp/new-releases/fashion/10509646011', category: 'Platform & Wedge Sandals' },
-  { url: 'https://www.amazon.com/gp/new-releases/fashion/11721155011', category: 'Knee-High Boots' }
+  { url: 'https://www.amazon.com/gp/new-releases/fashion/11721155011', category: 'Knee-High Boots' },
+  { url: 'https://www.amazon.com/gp/new-releases/fashion/679399011', category: 'Flats' }
 ] as const
